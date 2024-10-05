967e1bb3c7b549c2e0859e1eb7d9f60e7e401d52
thief01 <pst12345@wp.pl>
2024-10-05 02:05:18 +0200

    add tower followers

--- Assets/Scripts/Character/Behaviours/TowerBehaviour.cs
+++ Assets/Scripts/Character/Behaviours/TowerBehaviour.cs
@@ -1,9 +1,82 @@
+using System;
 using UnityEngine;
+using WRA.CharacterSystems.StatisticsSystem.Controlers;
+using WRA.CharacterSystems.StatisticsSystem.Statistics;
 
 namespace Character.Behaviours
 {
     public class TowerBehaviour : MonoBehaviour
     {
+        [SerializeField] private LayerMask enemyLayer;
+        private DynamicStatisticsController dynamicStatisticsController;
+        private DynamicStatisticValue attackRange;
+        private DynamicStatisticValue rotationSpeed;
         
+        private Collider2D[] enemiesInRange;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Update()
+        {
+            FindEnemies();
+            RotateTowardsEnemy();
+        }
+        
+        private void Init()
+        {
+            dynamicStatisticsController = GetComponent<DynamicStatisticsController>();
+            attackRange = dynamicStatisticsController.GetStatistic("AttackRange");
+            rotationSpeed = dynamicStatisticsController.GetStatistic("RotationSpeed");
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (Application.isEditor)
+            {
+                Init();
+            }
+            
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, attackRange.Value);
+        }
+
+        private void FindEnemies()
+        {
+            enemiesInRange = Physics2D.OverlapCircleAll(transform.position, attackRange.Value, enemyLayer);
+        }
+        
+        private void RotateTowardsEnemy()
+        {
+            var closestEnemy = GetClosestEnemy();
+            if (closestEnemy == null)
+            {
+                return;
+            }
+            
+            var direction = (closestEnemy.transform.position - transform.position).normalized;
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            var rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed.Value * Time.deltaTime);
+        }
+        
+        private Collider2D GetClosestEnemy()
+        {
+            Collider2D closestEnemy = null;
+            float closestDistance = Mathf.Infinity;
+            foreach (var enemy in enemiesInRange)
+            {
+                var distance = Vector2.Distance(transform.position, enemy.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestEnemy = enemy;
+                }
+            }
+
+            return closestEnemy;
+        }
     }
 }
