26d3fbfa9a508b687467d2811fefe5cd41d1bcd5
thief01 <pst12345@wp.pl>
2024-10-07 02:15:08 +0200

    add heal bar

--- Assets/Scripts/Character/CharacterBase.cs
+++ Assets/Scripts/Character/CharacterBase.cs
@@ -6,72 +6,12 @@ using WRA.CharacterSystems.StatisticsSystem.ResourcesInfos;
 
 namespace Character
 {
-    public abstract class CharacterBase : CharacterSystemBase, ICharacter, IPoolObject
+    public abstract class CharacterBase : CharacterSystemBase, ICharacter
     {
-        public UnityEvent<IPoolObject> OnKillEvent;
-        public UnityEvent<IPoolObject> OnSpawnEvent;
-        
-        public int VariantId
-        {
-            get => _variantId;
-            set => _variantId = value;
-        }
-
-        [SerializeField] private int _variantId;
-    
         public abstract void Move(Vector2 direction);
 
         public abstract void UseSkill(int skillIndex);
 
         public abstract void UpdateMousePosition(Vector2 mousePosition);
-
-        public abstract void OnInit();
-        
-        public abstract void OnSpawn();
-
-        public abstract void OnBeginKill(float delay);
-        
-        public abstract void OnKill();
-        
-        public void Spawn()
-        {
-            OnSpawn();
-            SetActive(true);
-        }
-    
-        public void Kill()
-        {
-            OnKill();
-            SetActive(false);
-            StopAllCoroutines();
-            OnKillEvent?.Invoke(this);
-        }
-
-        public void Kill(float delay)
-        {
-            OnBeginKill(delay);
-            StartCoroutine(KillDelay(delay));
-        }
-
-        public virtual void SetActive(bool active)
-        {
-            gameObject.SetActive(active);
-        }
-
-        private IEnumerator KillDelay(float delay)
-        {
-            float time = 0;
-            while (time < delay)
-            {
-                time += Time.deltaTime;
-                yield return null;
-            }
-            Kill();
-        }
-
-        public virtual void Kill(KillInfo killInfo)
-        {
-            Kill();
-        }
     }
 }
--- Assets/Scripts/Character/PlayerCharacter.cs
+++ Assets/Scripts/Character/PlayerCharacter.cs
@@ -18,25 +18,5 @@ namespace Character
         {
             throw new System.NotImplementedException();
         }
-
-        public override void OnInit()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void OnSpawn()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void OnBeginKill(float delay)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void OnKill()
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
