99db7c08d453e1e0234d56abdcc08b647146e19c
thief01 <pst12345@wp.pl>
2024-10-10 02:59:57 +0200

    earning money

--- Assets/Scripts/Character/Controllers/MoneyController.cs
+++ Assets/Scripts/Character/Controllers/MoneyController.cs
@@ -1,15 +1,30 @@
 using System;
 using UnityEngine;
 using WRA.CharacterSystems;
+using WRA.UI.PanelsSystem;
+using Zenject;
 
 namespace Character.Controllers
 {
     public class MoneyController : CharacterSystemBase
     {
-        public int Money { get; private set; }
+        public int Money
+        {
+            get => money;
+            private set
+            {
+                money = value;
+            }
+        }
     
-        [SerializeField] private int startMoney = 100;
-        
+        [SerializeField] private int money = 100;
+
+        [Inject] private PanelManager panelManager;
+        private void Start()
+        {
+            panelManager.OpenPanel("PlayerView", new PanelDataBase() {Data = this});
+        }
+
         public void AddMoney(int money)
         {
             Money += money;
