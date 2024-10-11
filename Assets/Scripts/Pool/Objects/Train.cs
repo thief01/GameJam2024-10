2886966f3b2b50c5f4b99e99c19fbb1b00143c6d
thief01 <pst12345@wp.pl>
2024-10-11 05:41:34 +0200

    remove throws

--- Assets/Scripts/Pool/Objects/Train.cs
+++ Assets/Scripts/Pool/Objects/Train.cs
@@ -8,22 +8,22 @@ namespace Pool.Objects
     {
         public override void OnInit()
         {
-            throw new System.NotImplementedException();
+            
         }
 
         public override void OnSpawn()
         {
-            throw new System.NotImplementedException();
+            
         }
 
         public override void OnBeginKill(float delay)
         {
-            throw new System.NotImplementedException();
+            
         }
 
         public override void OnKill()
         {
-            throw new System.NotImplementedException();
+            
         }
     }
 }
